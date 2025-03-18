1aef666709cb7066fe461f39b230bb744a36923a
brr-songko <syh6837@hanmail.net>
2025-03-18 18:00:51 +0900

    n14916 그리디

---
+++ src/main/java/com/brr/newcodingtest/n14916/Main2.java
@@ -0,0 +1,33 @@
+package com.brr.newcodingtest.n14916;
+
+import java.io.*;
+import java.util.*;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int max = Integer.MIN_VALUE;
+        boolean check = false;
+        for (int i = 0; i <= n / 5; i++) {
+            int temp = n;
+            int sum = 0;
+            sum += i;
+            temp -= 5*i;
+//            System.out.println("5가 " + i + "개일 때로 나눈 temp = " + temp);
+            sum += temp/2;
+            temp %= 2;
+//            System.out.println("2로 때로 나눈 temp = " + temp);
+//            System.out.println("sum = " + sum);
+            if (temp == 0) {
+                max = Math.max(max, sum);
+                check = true;
+            }
+        }
+        if (!check) {
+            System.out.println(-1);
+        } else {
+            System.out.println(max);
+        }
+    }
+}
