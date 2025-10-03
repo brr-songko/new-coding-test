8414c2e8259ab46f2d1b58ecbce9426e7fb4a2da
brr-songko <yhsong6837@gmail.com>
2025-10-03 17:23:18 +0900

    bfs 4회차 실패

---
+++ src/main/java/com/brr/newcodingtest/n2178/Main4.java
@@ -0,0 +1,57 @@
+package com.brr.newcodingtest.n2178;
+
+import java.io.*;
+import java.util.*;
+
+public class Main4 {
+    static int[] dy = {-1, 0, 1, 0};
+    static int[] dx = {0, 1, 0, -1};
+    static int N, M;
+    static boolean[][] visited;
+    static int[][] A;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        visited = new boolean[N][M];
+        A = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < M; j++) {
+                A[i][j] = Integer.parseInt(s.substring(j, j+ 1));
+            }
+        }
+
+        bfs(0, 0);
+
+        System.out.println(A[N-1][M-1]);
+    }
+
+    static void bfs(int a, int b) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{a, b});
+        visited[a][b] = true;
+
+        while (!queue.isEmpty()) {
+            int[] temp = queue.poll();
+            int y = temp[0];
+            int x = temp[1];
+
+            for (int i = 0; i < 4; i++) {
+                int ny = y + dy[i];
+                int nx = x + dx[i];
+
+                if (ny < 0 || ny >= N || nx < 0 || nx >= M) continue;
+                if (visited[ny][nx]) continue;
+                if (A[ny][nx] == 0) continue;
+
+                queue.offer(new int[]{ny, nx});
+                visited[ny][nx] = true;
+                A[ny][nx] = A[y][x] + 1;
+            }
+        }
+    }
+}
