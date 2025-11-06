4d5d3ebef2fa8105af701257fab3c9b19280b1ff
brr-songko <yhsong6837@gmail.com>
2025-11-06 17:58:16 +0900

    스택 진행중..

--- src/main/java/com/brr/newcodingtest/n2504/TempMain.java
+++ src/main/java/com/brr/newcodingtest/n2504/TempMain.java
@@ -8,35 +8,13 @@ public class TempMain {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
         Stack<Character> stack = new Stack<>();
-        Stack<Integer> intStack = new Stack<>();
-        Stack<String> stringStack = new Stack<>();
+        Stack<Integer> answerStack = new Stack<>();
 
         for (int i = 0; i < s.length(); i++) {
-            char c1 = s.charAt(i);
-            char c2 = s.charAt(i + 1);
-            if (i == 0 && c1 == ')' || c1 == ']') {
-                System.out.println(0);
-                break;
-            }
-            if (i == s.length() - 1 && c1 == '(' || c1 == '[') {
-                System.out.println(0);
-                break;
-            }
-            if (c1 == '(') {
-                if (c2 == ')') {
-                    intStack.push(2);
-                    stringStack.push("+");
-                } else if (c2 == ']') {
-                    System.out.println(0);
-                    break;
-                } else {
-                    intStack.push(2);
-                    stringStack.push("*");
-                }
-            } else if (c1 == '[' && c2 == ']') {
-                intStack.push(3);
-                stringStack.push("+");
-            } else if (c1 == '(') {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[') {
+                stack.push(c);
+            } else {
 
             }
         }
