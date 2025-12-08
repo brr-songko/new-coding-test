326d5fd355932dffe6a8925f5acaf05520a63303
brr-songko <yhsong6837@gmail.com>
2025-12-08 18:00:47 +0900

    dp 진행중

---
+++ src/main/java/com/brr/newcodingtest/n11048/Main.java
@@ -0,0 +1,49 @@
+package com.brr.newcodingtest.n11048;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[][] map, dp;
+    static int N, M;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        dp = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        dynamicP();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println(dp[N - 1][M - 1]);
+    }
+
+    static void dynamicP() {
+        dp[0][0] = map[0][0];
+        if (N > 1) {
+            dp[1][0] = map[0][0] + map[1][0];
+        }
+        if (M > 1) {
+            dp[0][1] = map[0][0] + map[0][1];
+        }
+
+        for (int i = 1; i < N; i++) {
+            for (int j = 1; j < M; j++) {
+                dp[i][j] = dp[i][j] + Math.max(Math.max(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]);
+            }
+        }
+    }
+}
