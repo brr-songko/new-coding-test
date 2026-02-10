01b656c7da3cf48f3f4851c933fb965d7ca4b456
songko <syh6837@hanmail.net>
2026-02-10 23:13:12 +0900

    시뮬 정답

--- src/main/java/com/brr/newcodingtest/n8911/Main.java
+++ src/main/java/com/brr/newcodingtest/n8911/Main.java
@@ -6,56 +6,30 @@ import java.util.*;
 public class Main {
     static int[] dy = {-1, 0, 1, 0};
     static int[] dx = {0, 1, 0, -1};
-    static int[][] map;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < T; i++) {
-            map = new int[500][500];
             String s = br.readLine();
             int y = 0;
             int x = 0;
             int d = 0;
-            int miny = Integer.MAX_VALUE;
-            int minx = Integer.MAX_VALUE;
-            int maxy = Integer.MIN_VALUE;
-            int maxx = Integer.MIN_VALUE;
+            int miny = 0;
+            int minx = 0;
+            int maxy = 0;
+            int maxx = 0;
 
             for (int j = 0; j < s.length(); j++) {
                 if (s.charAt(j) == 'F') {
                     y += dy[d];
                     x += dx[d];
 
-                    if (y < miny) {
-                        miny = y;
-                    }
-                    if (y > maxy) {
-                        maxy = y;
-                    }
-                    if (x < minx) {
-                        minx = x;
-                    }
-                    if (x > maxx) {
-                        maxx = x;
-                    }
                 } else if (s.charAt(j) == 'B') {
                     y += dy[(d + 2) % 4];
                     x += dx[(d + 2) % 4];
 
-                    if (y < miny) {
-                        miny = y;
-                    }
-                    if (y > maxy) {
-                        maxy = y;
-                    }
-                    if (x < minx) {
-                        minx = x;
-                    }
-                    if (x > maxx) {
-                        maxx = x;
-                    }
                 } else if (s.charAt(j) == 'R') {
                     d++;
                     if (d > 3) {
@@ -67,13 +41,15 @@ public class Main {
                         d = 3;
                     }
                 }
+
+                miny = Math.min(miny, y);
+                minx = Math.min(minx, x);
+                maxy = Math.max(maxy, y);
+                maxx = Math.max(maxx, x);
             }
 
-            int row = 0;
-            int col = 0;
-            if (miny > 0 && minx > 0) {
-                
-            }
+            int row = maxy - miny;
+            int col = maxx - minx;
 
             sb.append(row * col).append("\n");
         }
