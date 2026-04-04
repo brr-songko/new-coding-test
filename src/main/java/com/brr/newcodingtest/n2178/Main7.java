76941e6c46e0a721656e73e52c0d43d4d12a074a
songko <syh6837@hanmail.net>
2026-04-04 17:24:43 +0900

    bfs 성공

---
+++ src/main/java/com/brr/newcodingtest/n2178/Main7.java
@@ -0,0 +1,58 @@
+package com.brr.newcodingtest.n2178;
+
+import java.io.*;
+import java.util.*;
+
+public class Main7 {
+
+    static int N, M;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dy = {-1, 0, 1, 0};
+    static int[] dx = {0, 1, 0, -1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        visited = new boolean[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < M; j++) {
+                map[i][j] = s.charAt(j) - '0';
+            }
+        }
+
+        bfs(0, 0);
+
+        System.out.println(map[N - 1][M - 1]);
+    }
+
+    static void bfs(int r, int c) {
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{r, c});
+        visited[r][c] = true;
+
+        while (!q.isEmpty()) {
+            int[] temp = q.poll();
+            int y = temp[0];
+            int x = temp[1];
+
+            for (int i = 0; i < 4; i++) {
+                int ny = y + dy[i];
+                int nx = x + dx[i];
+
+                if (ny < 0 || ny >= N || nx < 0 || nx >= M) continue;
+                if (map[ny][nx] == 0) continue;
+                if (visited[ny][nx]) continue;
+
+                q.offer(new int[]{ny, nx});
+                map[ny][nx] = map[y][x] + 1;
+                visited[ny][nx] = true;
+            }
+        }
+    }
+}
