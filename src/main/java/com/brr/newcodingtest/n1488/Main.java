aa73514ca2ac85c7310dcb1022b12486a33a7398
songko <syh6837@hanmail.net>
2026-04-13 22:41:20 +0900

    시뮬 구현(애드훅) 실패

--- src/main/java/com/brr/newcodingtest/n1488/Main.java
+++ src/main/java/com/brr/newcodingtest/n1488/Main.java
@@ -25,49 +25,43 @@ public class Main {
         // [2] x, y의 의미 정하기
         // x = A 블록의 개수
         // y = B 블록의 개수
-        int x = 0;
-        int y = 0;
+        long x = 0;
+        long y = 0;
 
         // [3] x의 최댓값 구하기
         // A 블록이 x개 있으려면:
-        // 1) A 블록마다 A가 최소 1개씩 필요하다
-
-        // 2) A 블록들 사이를 B가 끊어줘야 한다
+        // 1) A 블록마다 A가 최소 1개씩 필요하다 x <= countA
+        // 2) A 블록들 사이를 B가 끊어줘야 한다 x <= countB + 1
         // 위 두 조건으로 x의 상한을 구해보기
-
-
-        // long x = TODO;
+        x = Math.min(countA, countB + 1);
 
         // [4] y의 최댓값 구하기
         // B 블록이 y개 있으려면:
         // 1) B 블록마다 B가 최소 1개씩 필요하다
         // 2) B 블록들 사이를 A가 끊어줘야 한다
         // 위 두 조건으로 y의 상한을 구해보기
-
-        // long y = TODO;
+        y = Math.min(countB, countA + 1);
 
         // [5] A를 실제로 최대 몇 개까지 쓸 수 있는지 구하기
         // x개의 A 블록이 있으면,
         // 각 블록에 A를 최대 maxA개까지 넣을 수 있다.
         // 즉 "A를 담을 수 있는 총 용량"을 먼저 구하고,
         // 실제 보유 개수 countA와 비교해서 더 작은 쪽을 사용한다.
-
-        // long aUsed = TODO;
+        long aUsed = Math.min(countA, x * maxA);
 
         // [6] B를 실제로 최대 몇 개까지 쓸 수 있는지 구하기
         // y개의 B 블록이 있으면,
         // 각 블록에 B를 최대 maxB개까지 넣을 수 있다.
         // 즉 "B를 담을 수 있는 총 용량"을 먼저 구하고,
         // 실제 보유 개수 countB와 비교해서 더 작은 쪽을 사용한다.
-
-        // long bUsed = TODO;
+        long bUsed = Math.min(countB, y * maxB);
 
         // [7] 최종 정답
         // 전체 문자열 길이 = 실제 사용한 A 개수 + 실제 사용한 B 개수
-
-        // long answer = TODO;
+        long answer = aUsed + bUsed;
 
         // [8] 출력
         // TODO
+        System.out.println(answer);
     }
 }
