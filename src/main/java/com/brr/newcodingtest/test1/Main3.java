501202d091f7181e5bb56f058c53dfe622776a23
songko <syh6837@hanmail.net>
2026-05-26 00:30:38 +0900

    dfs/bfs 1주차 테스트 2성공 1실패

---
+++ src/main/java/com/brr/newcodingtest/test1/Main3.java
@@ -0,0 +1,16 @@
+package com.brr.newcodingtest.test1;
+
+import java.io.*;
+import java.util.*;
+
+public class Main3 {
+
+    static int[] dy = {-1, 0, 1, 0};
+    static int[] dx = {0, 1, 0, -1};
+    static int[][][] map;
+    static boolean[][][] visited;
+
+    public static void main(String[] args) throws IOException {
+
+    }
+}
