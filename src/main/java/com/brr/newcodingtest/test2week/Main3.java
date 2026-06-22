e9b5117d301409c573403b4baaad1f0aecbb330c
songko <syh6837@hanmail.net>
2026-06-22 23:09:53 +0900

    스터디 코테

--- src/main/java/com/brr/newcodingtest/test2week/Main3.java
+++ src/main/java/com/brr/newcodingtest/test2week/Main3.java
@@ -16,17 +16,36 @@ class Tree {
 public class Main3 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         Map<String, Tree> map = new HashMap<>();
         String s;
-        while ((s = br.readLine()) != null) {
-            map.put(s, map.getOrDefault(s, new Tree(s, 0.0)));
+        while ((s = br.readLine()) != null && !s.isEmpty()) {
+            if (!map.containsKey(s)) {
+                map.put(s, new Tree(s, 0));
+            }
+            map.get(s).num += 1;
+            System.out.println("sdf");
         }
 
         double sum = 0.0;
-        for (double n : map.values()) {
-            sum += n;
+        for (Tree tree : map.values()) {
+            sum += tree.num;
         }
 
-        List<Tree> list = map.
+        List<Tree> list = new ArrayList<>(map.values());
+        Collections.sort(list, new Comparator<Tree>() {
+            @Override
+            public int compare(Tree t1, Tree t2) {
+                return t1.name.compareTo(t2.name);
+            }
+        });
+
+        for (Tree tree : list) {
+            sb.append(tree.name).append(" ");
+            double value = Math.round((tree.num / sum) * 10000) / 10000.0;
+            sb.append(value).append("\n");
+        }
+
+        System.out.println(sb);
     }
 }
